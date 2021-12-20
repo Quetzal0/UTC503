a34df850478c91afc9109730ef666f53eae36e49
Ewen <ewenbis829@gmail.com>
2021-12-20 16:33:19 +0100

    update status with photo: OK

--- twitter.py
+++ twitter.py
@@ -1,5 +1,7 @@
-import json
 import tweepy
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
 
 CONSUMER_KEY = 'LCIQ6UDScOWyQgMzxkqs3xl3P'
 CONSUMER_SECRET = 'SWetNtuZet5YWlPg91CYiB7HKAQS3yQnCDIWeJD0jSSlAJO4ai'
@@ -17,12 +19,42 @@ try:
 except:
     print("Error during authentication")
 
+def getdata(url): 
+    r = requests.get(url) 
+    return r.text 
+
+alt_array = []
+
+def images_alt():
+    htmldata = getdata("https://www.artmajeur.com/fr/oeuvres-d-art/sculpture/bronze-1432") 
+    soup = BeautifulSoup(htmldata, 'html.parser') 
+    images = soup.find_all('img',  attrs={'class': 'img-fluid'})
+
+    number = 0
+
+    for item in images:
+        if ('mini' in item['src']):
+            #print(item['alt'])
+            alt_array.append(item['alt'])
+            number += 1
+
+images_alt()
+
+#print(len(alt_array))
+
+print(alt_array[1])
+
+
 # the text to be tweeted
-status = "This is a tweet #3."
+status = alt_array[31   ]
+
+image_path = "D:\\Code\\Python\\UTC503\\31.jpg"
   
 # posting the tweet
-api.update_status(status)
+status = api.update_with_media(image_path, status)
 
 # posting confirmation
 print('Message posté')
+
+
 
