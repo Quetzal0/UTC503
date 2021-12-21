455e7b5dcefcf2909e09be11f08e898bbbb08063
Ewen <ewenbis829@gmail.com>
2021-12-21 16:42:10 +0100

    Automatic post : OK (Scheduled tasks)

--- twitter.py
+++ twitter.py
@@ -1,60 +1,78 @@
+from re import X
 import tweepy
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
-
-CONSUMER_KEY = 'LCIQ6UDScOWyQgMzxkqs3xl3P'
-CONSUMER_SECRET = 'SWetNtuZet5YWlPg91CYiB7HKAQS3yQnCDIWeJD0jSSlAJO4ai'
-ACCESS_TOKEN = '1465336751090712576-4CP1mSDmTq9LC31zQDsfYaj9nrPDNY'
-ACCESS_TOKEN_SECRET = 'H8RY13hE6kGkaOJsaatA9v1akoBkmOEJ6ryDmaK3dEB4H'
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
-
-def getdata(url): 
-    r = requests.get(url) 
-    return r.text 
+import random
+import config
 
 alt_array = []
 
-def images_alt():
-    htmldata = getdata("https://www.artmajeur.com/fr/oeuvres-d-art/sculpture/bronze-1432") 
+class Connection:
+    auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    try:
+        api.verify_credentials()
+        print('Authentication OK')
+    except:
+        print('Error during authentication')
+
+def getdata_html(url): 
+    r = requests.get(url) 
+    return r.text 
+
+def images_description():
+    htmldata = getdata_html('https://www.artmajeur.com/fr/oeuvres-d-art/sculpture/bronze-1432') 
     soup = BeautifulSoup(htmldata, 'html.parser') 
     images = soup.find_all('img',  attrs={'class': 'img-fluid'})
 
-    number = 0
+    number = 10
 
     for item in images:
         if ('mini' in item['src']):
-            #print(item['alt'])
             alt_array.append(item['alt'])
             number += 1
 
-images_alt()
+def publish():
 
-#print(len(alt_array))
+    random_num = random.randrange(10, 46)
+    print(random_num)
 
-print(alt_array[1])
+    newline = '\n' + str(random_num)
+
+    is_in = ''
+
+    with open('D:\\Code\\Python\\non_py\\history.txt', 'r') as f:
+        for line in f:
+            if str(random_num) in line:
+                is_in = 'true'
+                print(random_num)
+            else : 
+                pass
+    f.close
+
+    if is_in == 'true':
+        
+        print('already posted')
+        
+    else:
+
+        with open('D:\\Code\\Python\\non_py\\history.txt', 'a') as f:
+
+            f.write(str(newline))
+
+            images_description()
+
+            index = random_num - 10
+
+            status = alt_array[index]
+
+            image_path = 'D:\\Code\\Python\\UTC503\\' + str(random_num) + '.jpg'
+
+            status = Connection.api.update_with_media(image_path, status)
+
+            print('Posted')
 
 
-# the text to be tweeted
-status = alt_array[31   ]
-
-image_path = "D:\\Code\\Python\\UTC503\\31.jpg"
-  
-# posting the tweet
-status = api.update_with_media(image_path, status)
-
-# posting confirmation
-print('Message posté')
-
-
-
+publish()
