96bf67c776e067fafeddb9761d7862e65923422f
Ewen <ewenbis829@gmail.com>
2022-01-07 14:13:10 +0100

    modification of the code logic for more automation

--- twitter.py
+++ twitter.py
@@ -25,6 +25,20 @@ def getdata_html(url):
     r = requests.get(url) 
     return r.text 
 
+
+#check history of posts
+def history():
+    is_in = 'none'
+    random_num = random.randrange(10, 46)#ATTENTION
+    line_array = []
+    with open('D:\\Code\\Python\\non_py\\history.txt', 'r') as f:
+        for line in f:
+            num_in = int(line)
+            line_array.append(num_in)
+        while random_num in line_array:
+            random_num = random.randrange(10, 46)#ATTENTION
+    return random_num
+
 #get images description from HTML
 def images_description():
     htmldata = getdata_html('https://www.artmajeur.com/fr/oeuvres-d-art/sculpture/bronze-1432') 
@@ -40,36 +54,18 @@ def images_description():
 
 #publish downloaded images with description
 def publish():
-
-    random_num = random.randrange(10, 46)
-    print(random_num)
-
+    random_num = history()
     newline = '\n' + str(random_num)
-
-    is_in = ''
-
-    #check history of posts
-    with open('D:\\Code\\Python\\non_py\\history.txt', 'r') as f:
-        for line in f:
-            if str(random_num) in line:
-                is_in = 'true'
-                print(random_num)
-            else : 
-                pass
-    f.close
-
-    if is_in == 'true':
-        print('already posted')
     
     #publish images
-    else:
-        with open('D:\\Code\\Python\\non_py\\history.txt', 'a') as f:
-            f.write(str(newline))
-            images_description()
-            index = random_num - 10
-            status = alt_array[index]
-            image_path = 'D:\\Code\\Python\\UTC503\\' + str(random_num) + '.jpg'
-            status = Connection.api.update_with_media(image_path, status)
-            print('Posted')
+    with open('D:\\Code\\Python\\non_py\\history.txt', 'a') as f:
+        f.write(str(newline))
+        images_description()
+        index = random_num - 10
+        status = alt_array[index]
+        image_path = 'D:\\Code\\Python\\UTC503\\' + str(random_num) + '.jpg'
+        #Connection.api.update_with_media(image_path, status)
+        print('Posted : ' + str(random_num) + ' ' + alt_array[index])
+        return 0
 
 publish()
