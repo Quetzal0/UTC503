c3ce2b893ddc281e7c272adae1ca337e77cfa0ed
Quetzal0 <93988104+Quetzal0@users.noreply.github.com>
2022-01-24 09:13:55 +0100

    rm alt function

--- twitter.py
+++ twitter.py
@@ -5,9 +5,6 @@ from bs4 import BeautifulSoup
 import random
 import config
 
-#images description array
-alt_array = []
-
 #connection to twitter
 class Connection:
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
@@ -20,38 +17,28 @@ class Connection:
     except:
         print('Error during authentication')
 
+
+#images description array
+alt_array = []
+
 #get HTML data from URL
 def getdata_html(url): 
     r = requests.get(url) 
     return r.text 
 
-
 #check history of posts
 def history():
     is_in = 'none'
-    random_num = random.randrange(10, 46)#ATTENTION
+    random_num = random.randrange(10, 44)#ATTENTION
     line_array = []
-    with open('D:\\Code\\Python\\non_py\\history.txt', 'r') as f:
+    with open('D:\\Code\\Python\\UTC503\\history.txt', 'r') as f:
         for line in f:
-            num_in = int(line)
+            num_in = line
             line_array.append(num_in)
         f.close
         while random_num in line_array:
-            random_num = random.randrange(10, 46)#ATTENTION
+            random_num = random.randrange(10, 44)#ATTENTION
     return random_num
-
-#get images description from HTML
-def images_description():
-    htmldata = getdata_html('https://www.artmajeur.com/fr/oeuvres-d-art/sculpture/bronze-1432') 
-    soup = BeautifulSoup(htmldata, 'html.parser') 
-    images = soup.find_all('img',  attrs={'class': 'img-fluid'})
-
-    number = 10
-
-    for item in images:
-        if ('mini' in item['src']):
-            alt_array.append(item['alt'])
-            number += 1
 
 #publish downloaded images with description
 def publish():
@@ -59,14 +46,21 @@ def publish():
     newline = '\n' + str(random_num)
 
     #publish images
-    with open('D:\\Code\\Python\\non_py\\history.txt', 'a') as f:
+    with open('D:\\Code\\Python\\UTC503\\history.txt', 'a') as f:
         f.write(str(newline))
-        images_description()
-        index = random_num - 10
-        status = alt_array[index]
-        image_path = 'D:\\Code\\Python\\UTC503\\' + str(random_num) + '.jpg'
-        Connection.api.update_with_media(image_path, status)
-        print('Posted : ' + str(random_num) + ' ' + alt_array[index])
-        return 0
-
+        f.close
+    with open('D:\\Code\\Python\\UTC503\\alt.txt', 'r', encoding="utf-8") as f:
+        i = 0
+        for line in f:
+            print(f'{i}  : {line}')
+            alt_array.append(line)
+            i = i + 1
+        f.close
+    
+    index = random_num - 10
+    status = alt_array[index]
+    image_path = 'D:\\Code\\Python\\UTC503\\' + str(random_num) + '.jpg'
+    Connection.api.update_with_media(status, image_path)
+    #print(f'Posted :  {str(random_num)}  {alt_array[index]}')
+    return 0
 publish()
